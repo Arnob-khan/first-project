ac81c984177c16b17f227fd3e0cffc3f30123fd2
Arnob Khan <sayad.arnob@gmail.com>
2023-12-20 17:05:07 +0600

    done create students moduel

---
+++ src/app/modules/student.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import { studentService } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+    const result = await studentService.createStdudentIntoDB(studentData);
+
+    res.status(200).json({
+      success: true,
+      message: 'student is created successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStudent = async (req: Request, res: Response) => {
+  try {
+    const result = await studentService.getAllStudentFromDB();
+
+    res.status(201).json({
+      success: true,
+      message: 'students are retrived successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  const { studentId } = req.params;
+  const singleStudent = await studentService.getSingleStudentFromDB(studentId);
+
+  try {
+    res.status(200).json({
+      success: true,
+      message: 'students are retrived successfully',
+      data: singleStudent,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudent,
+  getSingleStudent,
+};
